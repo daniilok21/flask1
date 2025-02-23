6262d3a4fd1feb8ea4b8e4d6cd3a36f63d16cfee
Даниил <sergeevdaniil383@gmail.com>
2025-02-23 18:56:40 +0300

    21rf

--- 322/main.py
+++ 322/main.py
@@ -20,7 +20,7 @@ def form_sample():
                           </head>
                           <body>
                             <div class="text-center">                           
-                                <h1>Анкета претендета</h1>
+                                <h2>Анкета претендета</h2>
                                 <h4>на участии в миссии</h4>
                             </div>
                             <div>
@@ -216,6 +216,42 @@ def image_mars():
 
 @app.route('/promotion_image')
 def promotion_image():
+    return f"""<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+                        <link rel="stylesheet" 
+                        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                        <title>Колонизация</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="{url_for('static', filename='img/MARS.png')}" 
+           alt="здесь должна была быть картинка, но не нашлась">
+                        <div class="alert alert-secondary" role="alert">
+                          Человечество вырастает из детства.
+                        </div>
+                        <div class="alert alert-success" role="alert">
+                          Человечеству мала одна планета.
+                        </div>
+                        <div class="alert alert-secondary" role="alert">
+                          Мы сделаем обитаемыми безжизненные пока планеты.
+                        </div>
+                        <div class="alert alert-warning" role="alert">
+                          И начнем с Марса!
+                        </div>
+                        <div class="alert alert-danger" role="alert">
+                          Присоединяйся!
+                        </div>
+                      </body>
+                    </html>"""
+
+
+@app.route('/choice/<planet_name>')
+def choice(planet_name):
     return f"""<!doctype html>
                     <html lang="en">
                       <head>
